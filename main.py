786a9ded327f64f2ff550a1644cff2f28b6db650
Kartik <narkhede.1@iitj.ac.in>
2024-05-19 14:36:32 +0530

    adding dockerfile and render and requirements

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 import torch
 import torch.nn as nn
-import uvicorn
 import torch.nn.functional as F
 
 
@@ -431,6 +430,3 @@ async def chat(request: ChatRequest):
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
 
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
